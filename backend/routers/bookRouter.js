d2222be34db5b5ad04868ba65625dbba5d8465a2
Abdulmajid-Alhaj <abdemaged.alhaj123@gmail.com>
2024-07-26 17:48:44 +0300

    tareq

--- backend/routers/bookRouter.js
+++ backend/routers/bookRouter.js
@@ -4,6 +4,6 @@ const bookController = require('../controllers/booksControllers')
 
 router.get('/books',bookController.getAllBook)
 router.get('/catagory', bookController.getCatotgory)
-router.get('/book',bookController.forYou)
+router.get('/foryou',bookController.forYou)
 
 module.exports = router
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -114,7 +114,7 @@ const App = () => {
     }
   };
   useEffect(() => {
-    fetchProducts
+    fetchProducts,
     fetchMangaProducts
   },[])
 
